d2d9d01eb6b52287adeb71c6f4072646a5f95793
danibe248 <danibe248@gmail.com>
2016-10-30 22:21:13 +0100

    First commit

---
+++ NotSufficientImportException.java
@@ -0,0 +1,10 @@
+@SuppressWarnings("serial")
+public class NotSufficientImportException extends Exception {
+	public NotSufficientImportException() {
+		super();
+	}
+
+	public NotSufficientImportException(String message) {
+		super(message);
+	}
+}
